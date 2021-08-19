16907112ecbbcd77cdb62b2708175447e5dcf812
gautam351 <gautampraveen351@gmail.com>
2021-08-19 23:43:53 +0530

    initial

--- pages/Login.js
+++ pages/Login.js
@@ -10,7 +10,7 @@ function Login() {
     return (
         <div className="logincontainer">
             
-    <img src="https://www.google.com/logos/doodles/2020/stay-and-play-at-home-with-popular-past-google-doodles-cricket-2017-6753651837108767-2xa.gif"  className="logo" alt="google" />
+    {/* <img src="https://www.google.com/logos/doodles/2020/stay-and-play-at-home-with-popular-past-google-doodles-cricket-2017-6753651837108767-2xa.gif"  className="logo" alt="google" /> */}
         <a onClick={signin}>login with Google</a>      
     
      </div>
